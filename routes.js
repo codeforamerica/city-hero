1c5e5b1bd5ac78a0401beebfc27fd20cf651db9f
Alan Palazzolo <github@zzolo.org>
2011-05-10 15:24:15 -0700

    Added placeholder for home and project add pages.

--- routes.js
+++ routes.js
@@ -7,10 +7,20 @@
  * @param {object} App object (probably express)
  */
 function set_routes(app) {
+  
+  // Home
+  app.get('/', function(req, res) {
+    res.render('home.view.ejs');
+  });
 
   // Project page (this is currently just an example)
   app.get('/project', function(req, res) {
     res.render('project.view.ejs');
+  });
+  
+  // Project add page
+  app.get('/project/add', function(req, res) {
+    res.render('project-add.view.ejs');
   });
 };
 
