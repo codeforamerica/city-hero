5486be65ea8a2eec81482d0c65f419e77a481ce1
John Mertens <john.mertens@gmail.com>
2011-05-10 18:36:01 -0700

    Fix quotes on homepage. Fix model declaration.

--- models.js
+++ models.js
@@ -1,10 +1,16 @@
+/**
+ * @fileoverview Models for projects.
+ */
 
 cradle = require('cradle');
 
-var Model {
-}
-
 var Projects = {
+    
+    
+    
+    save: function(project, callback) {
+        
+    }
 }
 
 exports.Projects = Projects;
