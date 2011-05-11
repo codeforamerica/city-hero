434b029c5c20fdfafbb52cb2d35d12efb2643a12
Alan Palazzolo <github@zzolo.org>
2011-05-10 19:14:10 -0700

    Started adding some client JS for forms.

---
+++ static/js/forms.js
@@ -0,0 +1,10 @@
+/**
+ * Form jquery for client
+ */
+(function($) {
+  $(document).ready(function() {
+    // $('.form-item-help')....
+    
+    // do some form help stuff that is cool
+  });
+})(jQuery);
