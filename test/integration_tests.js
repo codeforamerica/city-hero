5374961a175b9c141f38f5ea320d3873f6e161f4
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2011-05-20 04:55:42 -0700

    Cleaned up the integration tests.

--- test/integration_tests.js
+++ test/integration_tests.js
@@ -1,12 +1,12 @@
 process.env.NODE_ENV = 'test';
 
 var assert = require('assert')
-  , app = require('server').app
   , cradle = require('cradle')
   , auth = require('settings/auth')
 
 module.exports = {
     'test that the home page gives us a 200 status' : function() {
+        var app = require('server').app
         assert.response(app,
             { url: '/', headers: {'Host': 'cityheroes.in'} },
             function(res) {
@@ -16,6 +16,7 @@ module.exports = {
     },
     
     'test that the add project form page gives us a 200 status' : function() {
+        var app = require('server').app
         assert.response(app,
             { url: '/projects/add', headers: {'Host': 'cityheroes.in'} },
             function(res) {
@@ -25,6 +26,7 @@ module.exports = {
     },
     
     'test that the project wizard page gives us a 200 status' : function() {
+        var app = require('server').app
         assert.response(app,
             { url: '/projects/wizard', headers: {'Host': 'cityheroes.in'} },
             function(res) {
@@ -34,30 +36,49 @@ module.exports = {
     },
     
     'test that an individual project page gives us a 200 status' : function() {
-        var conn = new (cradle.Connection)(auth.db.host, auth.db.port);
-        var db = conn.database('projects');
-        
-        // First let's create a project for us to use.
-        var project = {
-            _id: 'testProject1',
-            info: 'blah',
-            title: 'wonderful'
+        /**
+         * Set up fixture; create a project in the database.  This will be the
+         * Project whose project page we request.
+         */
+        function setup(test) {
+            var app = require('server').app
+            var conn = new (cradle.Connection)(auth.db.host, auth.db.port);
+            var db = conn.database('projects');
+            
+            var project = {
+                _id: 'testProject1',
+                info: 'blah',
+                title: 'wonderful'
+            }
+            
+            db.save(project, function(err, dbRes) {
+                assert.isNull(err);
+                test(app, db, dbRes);
+            });
         }
         
-        db.save(project, function(err, dbRes) {
+        /**
+         * Tear down fixture; clean up the database by removing the project we
+         * just created in set up.
+         */
+        function teardown(db, project) {
+            db.remove(project._id, project._rev);
+        }
         
+        /**
+         * Perform the test
+         */
+        setup(function(app, db, project) {
+        
+            // Check that the response has a status code of 200
             assert.response(app,
-                { url: '/projects/' + dbRes._id, headers: {'Host': 'cityheroes.in'} },
+                { url: '/projects/' + project._id, 
+                  headers: {'Host': 'cityheroes.in'} },
                 function(res) {
-//                    console.log(res.body);
                     assert.equal(res.statusCode, 200);
-            
-                    // Now get rid of that pesky testing project.
-                    db.remove(dbRes._id, dbRes._rev);
+                    
+                    teardown(db, project);
                 });
-            
-            console.log('The id and revision:')
-            console.log(dbRes._id + ', ' + dbRes._rev)
         });
     }
 }
