ad192c18137d6e112e778934682f2cd35c959169
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2011-05-31 17:13:51 -0700

    hide sidebar when certain tips pop up.

--- static/js/wayfinder.js
+++ static/js/wayfinder.js
@@ -1,5 +1,28 @@
+/**
+ * An object that maniplates the interface based on wayfinder commands.
+ */
 var projectPage = {
+    hideSideContent : function () {
+        var sideContent = $('#sidebar');
+        sideContent.css('visibility', 'hidden');
+    },
+    
+    showSideContent : function () {
+        var sideContent = $('#sidebar');
+        sideContent.css('visibility', 'visible');
+    },
+    
+    hideTips : function () {
+        var tips = $('.wizard-tip');
+        tips.hide();
+        this.showSideContent();
+    },
+    
+    showTip : function () {
+        this.hideSideContent()
+    },
+    
     addPhotograph : function () {
-        
+        this.hideSideContent();
     }
 }
